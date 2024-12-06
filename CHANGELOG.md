bfd71180fb8a32579cbaedea516d16ce14ed18e5
James Drake <info@jamesmdrakemedia.com>
2024-12-06 01:42:17 -0500

    updated docstrings and removed test func

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 ## Changelog
 
+### v1.1.1
+
+1. [Docs] Added docstrings to functions
+1. [Chore] Removed test function
+
 ### v1.1.0
 
 1. [Feature] Check for valid file path.
--- dedupe.py
+++ dedupe.py
@@ -3,10 +3,15 @@ import csv
 import hashlib
 from tqdm import tqdm
 
-VERSION_STRING: str = "1.1.0"
+VERSION_STRING: str = "1.1.1"
 
 
 def get_dir() -> str:
+    """Get the working directory from user input.
+
+    :return: Directory path
+    :rtype: str
+    """
     print("Enter or paste the directory you would like to work on below.")
     directory: str = input("Directory: ")
     if not os.path.exists(directory):
@@ -16,6 +21,13 @@ def get_dir() -> str:
 
 
 def read_files(working_directory: str) -> list:
+    """Read all files in the working directory. Add files and their MD5 hash to a list.
+
+    :param working_directory: String of the directory to scan.
+    :type working_directory: str
+    :return: List of files and their hashes, in the form of [[file, hash]].
+    :rtype: list
+    """
     hash_list: list = []
     for root, dirs, file in tqdm(os.walk(working_directory)):
         for f in file:
@@ -32,6 +44,13 @@ def read_files(working_directory: str) -> list:
 
 
 def find_duplicates(hash_list: list) -> list:
+    """Search through the list of files for duplicates.
+
+    :param hash_list: List of files and their hashes, in the form of [[file, hash]].
+    :type hash_list: list
+    :return: List of duplicated files, in the form of [[file, hash]].
+    :rtype: list
+    """
     seen: dict = {}
     duplicated: dict = {}
 
@@ -51,6 +70,15 @@ def find_duplicates(hash_list: list) -> list:
 
 
 def make_report(directory: str, duplicated_files: list) -> bool:
+    """Creates a .csv file in the root working directory containing the list of duplicated files and their hashes.
+
+    :param directory: Root of working directory.
+    :type directory: str
+    :param duplicated_files: List of duplicated files, in the in the form of [[file, hash]].
+    :type duplicated_files: list
+    :return: Returns True or False based on the success of writing the file.
+    :rtype: bool
+    """
     title_row: list = ["Filename", "Checksum"]
     filename: str = os.path.join(directory, "duplicated_hash_list.csv")
 
@@ -62,13 +90,6 @@ def make_report(directory: str, duplicated_files: list) -> bool:
         return True
     except IOError:
         return False
-
-
-def test_hash_list(directory, hash_list) -> None:
-    filename = os.path.join(directory, "hash_list.csv")
-    with open(filename, "w") as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerows(hash_list)
 
 
 if __name__ == "__main__":
